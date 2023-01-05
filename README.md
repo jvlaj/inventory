b7ecf4c125aef1b9b0bbfc6c39a80b4911071b13
Jason Vlajankov <jvlaj@protonmail.com>
2023-01-05 17:03:10 +1100

    updated displays of django admin - part 4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # inventory
 An inventory system developed using Django.
+
+Following [Mozilla Django Tutorial](https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django)
--- home/admin.py
+++ home/admin.py
@@ -3,19 +3,44 @@ from django.contrib import admin
 from home.models import Genre, Book, BookInstance, Author
 
 
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
+
+class BookInstanceInLine(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back', 'id')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display =  ('title', 'author', 'display_genre')
+    list_display = ('title', 'author', 'display_genre')
 
-
-@admin.register(BookInstance)
-class BookInstance(admin.ModelAdmin):
-    list_display = ('book', 'status', 'due_back')
+    inlines = [BookInstanceInLine]
 
 
 admin.site.register(Genre)
