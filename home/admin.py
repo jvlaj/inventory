a7c6138167dffc7060d99edaa9e41306994b7286
Jason Vlajankov <jvlaj@protonmail.com>
2023-01-05 15:50:59 +1100

    Added libary models; Updated list_display for admin site

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from home.models import Genre, Book, BookInstance, Author
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display =  ('title', 'author', 'display_genre')
+
+
+@admin.register(BookInstance)
+class BookInstance(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')
+
+
+admin.site.register(Genre)
