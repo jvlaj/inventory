126920e8a614e91f10bf26b7af300c256f7667ad
Jason Vlajankov <jvlaj@protonmail.com>
2023-01-06 11:23:04 +1100

    Updated index template and views for library homepage

--- home/views.py
+++ home/views.py
@@ -1,7 +1,26 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 
-def home(request):
-    # return render(request, template_name='<h1>Welcome to homepage</h1')
-    return HttpResponse('<h1>Welcome</h1>')
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # all() is implied by default
+    num_authors = Author.objects.count()
+
+    num_skull = Book.objects.filter(title__contains='skul').count()
+    num_fiction = Genre.objects.filter(book__genre__name__contains='fiction').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_fiction': num_fiction,
+        'num_skull': num_skull,
+    }
+
+    return render(request, 'index.html', context)
