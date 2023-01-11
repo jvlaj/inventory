0a51197dac33cce1f710617388cd718daea6dc20
Jason Vlajankov <jvlaj@protonmail.com>
2023-01-11 22:56:16 +1100

    Tests for forms, models and views of Django web application

--- home/views.py
+++ home/views.py
@@ -113,18 +113,19 @@ class AllLoanedBooksByListView(LoginRequiredMixin, PermissionRequiredMixin, gene
         return BookInstance.objects.filter(status__exact='o', ).order_by('due_back')
 
 
-class AuthorCreate(CreateView):
+class AuthorCreate(PermissionRequiredMixin, CreateView):
     model = Author
     fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
     initial = {'date_of_death': '11/06/2020'}
+    permission_required = 'home.can_mark_returned'
 
 
-class AuthorUpdate(UpdateView):
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
     model = Author
     fields = "__all__"
 
 
-class AuthorDelete(DeleteView):
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
     model = Author
     success_url = reverse_lazy('authors')
 
